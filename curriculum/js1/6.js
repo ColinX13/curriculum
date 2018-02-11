190c3aa895ca60da05d84a4e256d394c03798021
Colin Xie <colinxie99@gmail.com>
2018-02-10 22:56:29 -0800

    solved problem 6

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,8 +5,14 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, b = a, sum = 0) => {
+  if(a === 2) {
+    return sum;
+  }
+  if(b % (a - 1) === 0) {
+    sum += (a - 1);
+  }
+  return solution(a - 1, b, sum);
 };
 
 module.exports = {
